d01de5eab233c2f7a7109719f59d278474c51e1a
KimBlazter <blasster35@gmail.com>
2025-11-25 01:56:19 +0100

    fix(components): integrate Tooltip for tab titles in TabSwitcher

--- src/components/ui/TabSwitcher.tsx
+++ src/components/ui/TabSwitcher.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import { ReactNode, useState } from "react";
 import ItemIcon from "../ItemIcon";
 import { Texture } from "@/utils/spriteLoader";
+import { Tooltip } from "./Tooltip";
 
 namespace TabSwitcher {
     export type Tab = {
@@ -23,29 +24,29 @@ export default function TabSwitcher(props: TabSwitcher.Props) {
     return (
         <div className={clsx("flex w-full flex-col", props.className)}>
             {/* Tabs */}
-            <div className="flex h-12 flex-row items-center">
+            <div className="-mb-3 flex flex-row items-center">
                 {props.tabs.map((tab, index) => (
-                    <div key={index} className="relative w-full">
-                        <div
-                            onClick={() => setCurrentTab(index)}
-                            className={clsx(
-                                "inventory-border flex w-full flex-row items-center gap-1 border-b-0",
-                                currentTab === index ? "z-5" : "brightness-85"
-                            )}
-                        >
-                            <ItemIcon texture={tab.icon} className="h-10" />
-                            <span className="h-auto text-black">
+                    // Tab
+                    <div
+                        key={index}
+                        onClick={() => setCurrentTab(index)}
+                        className={clsx(
+                            "inventory-border relative border-b-0 pb-3",
+                            currentTab === index ? "z-6" : "brightness-85"
+                        )}
+                    >
+                        <div className="flex w-full flex-row items-center">
+                            <Tooltip
+                                key={index}
+                                content={tab.title}
+                                position="top"
+                            >
+                                <ItemIcon texture={tab.icon} className="h-10" />
+                            </Tooltip>
+
+                            {/* <span className="h-auto text-black">
                                 {tab.title}
-                            </span>
-                        </div>
-                        <div
-                            className={clsx(
-                                "inventory-border absolute -bottom-2 z-4 h-2 w-full border-y-0",
-                                currentTab === index && "z-6"
-                            )}
-                        ></div>
-                        <div className="absolute -bottom-4 z-7 h-2 w-full px-2">
-                            <div className="bg-mcInventoryBackground h-full w-full"></div>
+                            </span> */}
                         </div>
                     </div>
                 ))}
