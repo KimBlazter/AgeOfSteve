d0c6dbe389701558d9f65d2f7fb84e7d5ca7acf4
KimBlazter <blasster35@gmail.com>
2025-11-25 10:57:07 +0100

    fix(components): remove ResourcesComponent from InventoryComponent and adjust layout

--- src/components/InventoryComponent.tsx
+++ src/components/InventoryComponent.tsx
@@ -1,16 +1,10 @@
-import ResourcesComponent from "@/components/resources/ResourcesComponent";
 import ItemsComponent from "@/components/items/ItemsComponent";
 
 export default function InventoryComponent() {
     return (
         <div className="inventory-border text-mcInventoryText flex h-full flex-row gap-2">
-            {/* Resources */}
-            <div className="h-full w-3/7">
-                <ResourcesComponent />
-            </div>
-
             {/* Items */}
-            <div className="h-full w-4/7">
+            <div className="h-full w-full">
                 <ItemsComponent />
             </div>
         </div>
